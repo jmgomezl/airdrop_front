7f6c2f5a2712ca06aad7c794e7367096ea7911e8
Yeison Andres Hernandez <desarrollojava@ada.co>
2024-09-26 07:50:45 -0500

    proyecto base front

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import NavbarComponent from "./components/navbar/NavbarComponent"
+import LoginPage from "./pages/login/LoginPage"
+
+function App() {
+
+  return (
+   <>
+   <NavbarComponent /> 
+    <LoginPage />
+   </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/NavbarComponent.tsx
@@ -0,0 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+const NavbarComponent = () => {
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+    <Container>
+      <Navbar.Brand href="#">Airdrop</Navbar.Brand>
+      <Nav  className="justify-content-end">
+            <Nav.Link href="#home">Login</Nav.Link>
+            <Nav.Link href="#features">Registro</Nav.Link>
+          </Nav>
+    </Container>
+  </Navbar>
+  )
+}
+
+export default NavbarComponent
